8ac6a0bd4b9a49bc5fccfc3f7a73b8f302773567
Slushisquid <150743764+Slushisquid@users.noreply.github.com>
2024-09-05 10:39:21 -0400

    GOTTA SAVE THIS RN

---
+++ fizzbuzz.js
@@ -0,0 +1,10 @@
+//number 1-100
+//multiples of 3 print fizz
+//multiples of 5 print buzz
+//multiples of 15 print fizzbuzz
+
+for (i=1; i<101; i++){
+    if (i%15 == 0){
+        //NOT DONE
+    }
+}
