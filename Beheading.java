d6b77b750e13ed7e3fbc5780b44627342610ef51
Slushisquid <150743764+Slushisquid@users.noreply.github.com>
2024-09-06 19:40:31 +0000

    syntax moment

---
+++ Beheading.java
@@ -0,0 +1,11 @@
+public class Beheading
+{
+	public static void main ( String[] args )
+	{
+
+		System.out.println("  O    ");
+        System.out.println("   -|-");
+        System.out.println("   /\\");
+        System.out.println(" BEHEADED MAN <3");
+}
+}
--- fruit.java
+++ fruit.java
@@ -1,34 +1,37 @@
 class Main {
-    public static void main(String[] args) {
-      // Write your code here 
-      //YELLOW FRUITS!
-  String [] flavors = {"Banana", "Lemon", "Pineapple", "Banana", "Lemon", "Mango", "Mango", "Lemon", "Durian", "Quince", "Papaya", "Banana", "Lemon", "Pineapple", "Corn", "Salmon Berry", "Golden Raspberry"};
+  public static void main(String[] args) {
+    // Write your code here 
+    //YELLOW FRUITS!
+String [] flavors = {"Banana", "Lemon", "Pineapple", "Banana", "Lemon", "Mango", "Mango", "Lemon", "Durian", "Quince", "Papaya", "Banana", "Lemon", "Pineapple", "Corn", "Salmon Berry", "Golden Raspberry"};
+
+    int bananacount = 0;
+    int lemoncount = 0;
+    int pineapplecount = 0;
+    int mangocount = 0;
+    int othercount = 0;
+    
+for (String flavor: flavors) {
+  if (flavor.equals("Banana")) {
+    bananacount ++;
+  } else if (flavor.equals("Lemon")) {
+    lemoncount ++;
+  } else if (flavor.equals("Pineapple")) {
+    pineapplecount ++;
   
-      int bananacount = 0;
-      int lemoncount = 0;
-      int pineapplecount = 0;
-      int mangocount = 0;
-      int othercount = 0;
-      
-  for (flavor: flavors) {
-    if (flavor .equals) "Banana" {
-      bananacount ++;
-    } else if (flavor.equals) "Lemon" {
-      lemoncount ++;
-    } else if (flavor.equals) "Pineapple" {
-      pineapplecount ++;
-    } else if (flavor.equals) "Mango" {
-      mangocount ++;
-    } else {
-      othercount ++;
-    }
-    }
+  } else if (flavor.equals("Mango")) {
+    mangocount ++;
+  } else {
+    othercount ++;
   }
-  
-  System.out.println(" There were " + bananacount + " bananas.");
-  System.out.println(" There were " + lemoncount + " lemons.");
-  System.out.println(" There were " + pineapple count + " pineapples.");
-  System.out.println(" There were " + mangocount + " mangoes.");
-  System.out.println(" There were " + othercount + " other yellow fruits.");
-    }
+}
+System.out.println(" There were " + bananacount + " bananas.");
+System.out.println(" There were " + lemoncount + " lemons.");
+System.out.println(" There were " + pineapplecount + " pineapples.");
+System.out.println(" There were " + mangocount + " mangoes.");
+System.out.println(" There were " + othercount + " other yellow fruits.");
+}
+
+
   }
+
+  //thank you bestie Katie :)))))))))))))
