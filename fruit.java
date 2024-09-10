3a028ecea245d8fcf2b266219fad2d94577ebdf1
Slushisquid <150743764+Slushisquid@users.noreply.github.com>
2024-09-10 01:41:54 +0000

    i heart hash tablez

--- fruit.java
+++ fruit.java
@@ -1,37 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Main {
   public static void main(String[] args) {
-    // Write your code here 
     //YELLOW FRUITS!
-String [] flavors = {"Banana", "Lemon", "Pineapple", "Banana", "Lemon", "Mango", "Mango", "Lemon", "Durian", "Quince", "Papaya", "Banana", "Lemon", "Pineapple", "Corn", "Salmon Berry", "Golden Raspberry"};
+    String [] flavors = {"Banana", "Lemon", "Pineapple", "Banana", "Lemon", "Mango", "Mango", "Lemon", "Durian", "Quince", "Papaya", "Banana", "Lemon", "Pineapple", "Corn", "Salmon Berry", "Golden Raspberry"};
 
-    int bananacount = 0;
-    int lemoncount = 0;
-    int pineapplecount = 0;
-    int mangocount = 0;
-    int othercount = 0;
+    HashMap<String, Integer> fruitz = new HashMap<>();
     
-for (String flavor: flavors) {
-  if (flavor.equals("Banana")) {
-    bananacount ++;
-  } else if (flavor.equals("Lemon")) {
-    lemoncount ++;
-  } else if (flavor.equals("Pineapple")) {
-    pineapplecount ++;
-  
-  } else if (flavor.equals("Mango")) {
-    mangocount ++;
-  } else {
-    othercount ++;
+    for (String flavor: flavors) {
+      if (fruitz.containsKey(flavor)) {
+        fruitz.put(flavor, fruitz.get(flavor) + 1);
+      } else {
+        fruitz.put(flavor, 1);
+      }
+    }
+    for (Map.Entry<String, Integer> entry : fruitz.entrySet()) {
+      System.out.println(entry.getKey() + ": " + entry.getValue());
+    }
   }
-}
-System.out.println(" There were " + bananacount + " bananas.");
-System.out.println(" There were " + lemoncount + " lemons.");
-System.out.println(" There were " + pineapplecount + " pineapples.");
-System.out.println(" There were " + mangocount + " mangoes.");
-System.out.println(" There were " + othercount + " other yellow fruits.");
-}
 
 
-  }
+}
 
   //thank you bestie Katie :)))))))))))))
